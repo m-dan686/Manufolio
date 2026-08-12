48d0c2fa827834a0967008122d0d3e466a4ab30e
m_dan686 <43manuanandan12a@gmail.com>
2026-08-12 09:49:30 +0530

    chore: clean and prepare Manufolio for deployment

--- frontend/src/components/animations/ParticleBackground.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ParticleBackground = () => {
-    // Disabled random floating pastel green/yellow particle shapes to maintain a clean, premium visual design.
-    return null;
-};
-
-export default ParticleBackground;
